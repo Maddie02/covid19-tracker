34d70fed8283bb72ddb508f27435c0b52c49f692
Madlen <madlensarkisian@gmail.com>
2020-05-26 11:22:24 +0300

    [Update] Implemented card UI with fetched real data

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import styles from './App.module.css';
 import { fetchData } from './api/index';
 
 class App extends React.Component {
+
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
